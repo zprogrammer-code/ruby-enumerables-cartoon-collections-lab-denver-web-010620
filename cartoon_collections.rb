97832f990b7a668d44705295437451d8d8a93f57
Zachary Jacobs <skatboyza124@hotmail.com>
2019-12-07 01:35:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,7 @@ def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 while i < array.length do
 if array.include?("cheddar")
-  return "#{array[i].capitalize}!" 
+  return "#{cheese_types[i].capitalize}!" 
 end
 i += 1 
 end
